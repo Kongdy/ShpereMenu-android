58f238b47be4ca1e0d0cb8885031cbe855d38095
unknown <>
2016-05-21 01:06:27 +0800

    Initial commit

---
+++ app/src/main/java/com/kongdy/hasee/shperemenu_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kongdy.hasee.shperemenu_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/kongdy/hasee/shperemenu_android/view/SphereMenu.java
@@ -0,0 +1,23 @@
+package com.kongdy.hasee.shperemenu_android.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kongdy on 2016/5/21.
+ */
+public class SphereMenu extends View {
+
+    public SphereMenu(Context context) {
+        super(context);
+    }
+
+    public SphereMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SphereMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
