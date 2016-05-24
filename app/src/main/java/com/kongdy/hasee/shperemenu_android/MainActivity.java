a5d029093cf4ee338a62e1d9f56f90a7d8287a2c
Kongdy <906011748@qq.com>
2016-05-24 23:54:28 +0800

    mult move success

--- app/src/main/java/com/kongdy/hasee/shperemenu_android/MainActivity.java
+++ app/src/main/java/com/kongdy/hasee/shperemenu_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.kongdy.hasee.shperemenu_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.kongdy.hasee.shperemenu_android.view.SphereMenu;
 
@@ -10,6 +13,7 @@ import com.kongdy.hasee.shperemenu_android.view.SphereMenu;
 public class MainActivity extends AppCompatActivity {
 
     private SphereMenu m_sphere_m;
+    private SphereMenu.onSphereMenuItemClickListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         m_sphere_m = (SphereMenu) findViewById(R.id.m_sphere_m);
-//        ImageView iv1 = new ImageView(getApplicationContext());
-//        ImageView iv2 = new ImageView(getApplicationContext());
-//        ImageView iv3 = new ImageView(getApplicationContext());
-//        ImageView iv4 = new ImageView(getApplicationContext());
-//
-//        iv1.setImageResource(R.mipmap.m_1);
-//        iv2.setImageResource(R.mipmap.m_2);
-//        iv3.setImageResource(R.mipmap.m_3);
-//        iv4.setImageResource(R.mipmap.m_4);
-//
-//        m_sphere_m.addView(iv1);
-//        m_sphere_m.addView(iv2);
-//        m_sphere_m.addView(iv3);
-//        m_sphere_m.addView(iv4);
+        listener = new SphereMenu.onSphereMenuItemClickListener() {
+            @Override
+            public void onClick(View v, int pos) {
+                Log.v("main_click","pos:"+pos);
+                Toast.makeText(getApplicationContext(),"click pos:"+pos,Toast.LENGTH_SHORT).show();
+            }
+        };
+        m_sphere_m.addOnSphereMenuClickListener(listener);
 
     }
 }
--- app/src/main/java/com/kongdy/hasee/shperemenu_android/view/SphereMenu.java
+++ app/src/main/java/com/kongdy/hasee/shperemenu_android/view/SphereMenu.java
@@ -1,20 +1,21 @@
 package com.kongdy.hasee.shperemenu_android.view;
 
+import android.animation.Animator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.view.animation.BounceInterpolator;
-import android.view.animation.TranslateAnimation;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by kongdy on 2016/5/21.
@@ -23,8 +24,12 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
 
     private float radius = 0;
     private MENU_STATUS MenuStatu = MENU_STATUS.CLOSE;
-    private View moveView;
-    private Point movePoint;
+
+    private boolean beginDrag = false;
+    private onSphereMenuItemClickListener listener;
+    private Map<Integer,Integer> movingPoss; // moving view
+    private Map<Integer,Point> movePoints;
+    CircleImageView homeBtn; // home btn
 
     public enum MENU_STATUS{
         OPEN,
@@ -45,7 +50,8 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
 
     private void initView() {
         setBackgroundColor(Color.argb(150,255,255,0));
-        movePoint = new Point();
+        movePoints = new HashMap<>();
+        movingPoss = new HashMap<>();
     }
 
     private void unfoldAnimation(final View view,int i) {
@@ -65,68 +71,26 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
                 invalidate();
             }
         });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                homeBtn.setEnabled(true);
+                view.setEnabled(true);
+                view.setFocusable(true);
+                view.setClickable(true);
+            }
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
         animator.setDuration(1000);
         animator.start();
-//        final float speedX = 1000/toX;
-//        final float speedY = 1000/toY;
-//        final long unitTime = (long) (1000/radius);
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                float x = view.getX();
-//                float y = view.getY();
-//                while(x < orgX+toX && y < orgY+toY) {
-//                    long startTime = System.currentTimeMillis();
-//                    view.layout((int)(x+5),(int)(y+5),(int)(x+5+view.getMeasuredWidth())
-//                    ,(int)(y+5+view.getMeasuredHeight()));
-//                    Log.v("thread move","view.getX():"+view.getX()+",view.getY():"+view.getY());
-//                    x = view.getX();
-//                    y = view.getX();
-//                    long endTime = System.currentTimeMillis();
-//                    invalidate();
-//                    if(endTime - startTime < 50) {
-//                        try {
-//                            Thread.sleep(50-(endTime - startTime));
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//            }
-//        });
-//        thread.run();
-//        AnimationSet animationSet = new AnimationSet(true);
-//        Animation translateAnimation = new TranslateAnimation(0,toX,0,-toY);
-//        translateAnimation.setStartOffset(i*50);
-//        translateAnimation.setFillAfter(true);
-//        translateAnimation.setFillEnabled(true);
-//        animationSet.addAnimation(translateAnimation);
-//        animationSet.setDuration(1000);
-//        animationSet.setFillAfter(true);
-//        animationSet.setFillEnabled(true);
-//        animationSet.setInterpolator(new BounceInterpolator());
-//        animationSet.setAnimationListener(new Animation.AnimationListener() {
-//            @Override
-//            public void onAnimationStart(Animation animation) {
-//            }
-//
-//            @Override
-//            public void onAnimationEnd(Animation animation) {
-//                // update coord
-////                view.layout((int)(view.getX()+toX),(int)(view.getY()-toY),
-////                        (int)(view.getX()+toX+view.getMeasuredWidth())
-////                        ,(int)(view.getY()-toY+view.getMeasuredHeight()));
-//                view.layout((int)(view.getX()),(int)(view.getY()),
-//                        (int)(view.getX()+view.getMeasuredWidth())
-//                        ,(int)(view.getY()+view.getMeasuredHeight()));
-//                Log.v("ac","view.getLeft():"+view.getLeft()+",view.getTop():"+view.getTop());
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation animation) {
-//            }
-//        });
-//        animationSet.setStartOffset((i*100)/getChildCount());
         MenuStatu = MENU_STATUS.OPEN;
     }
 
@@ -134,50 +98,96 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
                 for (int i = 1;i < getChildCount();i++) {
                     final View view = getChildAt(i);
-                    if(isCrash(view,event)) {
-                        Toast.makeText(getContext(),"touch position"+i,Toast.LENGTH_SHORT).show();
-                        moveView = view;
-                        movePoint.x = (int) event.getX();
-                        movePoint.y = (int) event.getY();
+                    if(isCrash(view,event) && !movingPoss.containsValue(i)) {
+                        final int actionIndex = event.getActionIndex();
+                        Point tempP = new Point();
+                        tempP.x = (int) event.getX(actionIndex);
+                        tempP.y = (int) event.getY(actionIndex);
+                        movingPoss.put(actionIndex,i);
+                        movePoints.put(actionIndex,tempP);
                     }
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
-                if(moveView != null) {
-                    final int tempX = (int) (event.getX()-movePoint.x);
-                    final int tempY = (int) (event.getY()-movePoint.y);
-                    moveView.layout((int)(moveView.getX()+tempX),(int)(moveView.getY()+tempY),
-                            (int)(moveView.getX()+moveView.getWidth()+tempX)
-                    ,(int)(moveView.getY()+moveView.getHeight()+tempY));
-                    movePoint.x = (int) event.getX();
-                    movePoint.y = (int) event.getY();
+                Set<Integer> keys = movingPoss.keySet();
+                for (int i:keys) {
+                    beginDrag = true;
+                    final int tempX = (int) (event.getX(i) - movePoints.get(i).x);
+                    final int tempY = (int) (event.getY(i) - movePoints.get(i).y);
+                    final View view = getChildAt(movingPoss.get(i));
+                    if (view != null) {
+                        int l = view.getLeft();
+                        int t = view.getTop();
+                        view.layout(l + tempX, t + tempY,
+                                l + view.getMeasuredWidth() + tempX
+                                , t + view.getMeasuredHeight() + tempY);
+                        movePoints.get(i).x = (int) event.getX(i);
+                        movePoints.get(i).y = (int) event.getY(i);
+                    }
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                moveView = null;
+            case MotionEvent.ACTION_POINTER_UP:
+                final int lostTouchIndex = event.getActionIndex();
+                if(movingPoss.containsKey(lostTouchIndex)) {
+                    final int pos = movingPoss.get(lostTouchIndex);
+                    final View view = getChildAt(pos);
+                    if(movingPoss.size() <= 1 && !beginDrag) {
+                        if(listener != null) {
+                            listener.onClick(view,pos);
+                        }
+                        closeMenu();
+                    } else if(beginDrag){
+                        backAnimation(view,view.getX(),view.getY(),pos);
+                    }
+                    movingPoss.remove(lostTouchIndex);
+                    movePoints.remove(lostTouchIndex);
+                }
+                if(movingPoss.size() < 1) {
+                    beginDrag = false;
+                }
+
                 break;
         }
         return true;
     }
 
-    public Animation backAnimation(int pos,float touchX,float touchY) {
+    public void backAnimation(final View v,float touchX,float touchY,int pos) {
         float angle = (float) ((Math.PI/(getChildCount()))*pos-Math.PI/2);
         float backX = (float) (Math.sin(angle)*radius);
-        float backY = (float) Math.cos(angle) *radius;
-        AnimationSet animationSet = new AnimationSet(true);
-        Animation transAnimation = new TranslateAnimation(touchX,backX,touchY,backY);
-        animationSet.addAnimation(transAnimation);
-        animationSet.setDuration(500);
-        animationSet.setFillAfter(true);
-        return animationSet;
+        float backY = (float) (Math.cos(angle) *radius);
+        /*
+         * calculate org pos
+         */
+        int l = (int) ((getMeasuredWidth()-v.getMeasuredWidth())/2+backX);
+        int t = (int) (getMeasuredHeight()-v.getMeasuredHeight()-backY);
+
+        PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat("x",touchX,l);
+        PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat("y",touchY,t);
+
+        ValueAnimator animator = ValueAnimator.ofPropertyValuesHolder(holderX,holderY);
+        animator.setInterpolator(new BounceInterpolator());
+        animator.setDuration(800);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float x = (float) animation.getAnimatedValue("x");
+                float y = (float) animation.getAnimatedValue("y");
+                v.setX(x);
+                v.setY(y);
+                invalidate();
+            }
+        });
+        animator.start();
     }
 
     public void foldAnimation(final View view, int i) {
         final float angle = (float) ((Math.PI/(getChildCount()))*i-Math.PI/2);
         final float fromX = (float) (Math.sin(angle)*radius);
-        final float fromY = (float) (Math.cos(angle) *radius);
+        final float fromY = (float) (Math.cos(angle)*radius);
         PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat("x", view.getX(),view.getX()-fromX);
         PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat("y", view.getY(),view.getY()+fromY);
         PropertyValuesHolder holderAlpha = PropertyValuesHolder.ofFloat("a", 1f,0f);
@@ -195,6 +205,25 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
                     view.setAlpha(0);
                 }
                 invalidate();
+            }
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                homeBtn.setEnabled(true);
+                view.setEnabled(false);
+                view.setVisibility(View.GONE);
+                view.setFocusable(false);
+                view.setClickable(false);
+            }
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
             }
         });
         animator.setDuration(1000);
@@ -234,25 +263,19 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
             }
         }
         radius = getMeasuredWidth()/2-childWidth;
-        CircleImageView homeBtn = (CircleImageView) getChildAt(0);
+        homeBtn = (CircleImageView) getChildAt(0);
         homeBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Log.d("sphere","home click");
         final int count = getChildCount();
         if(count > 1) {
+            homeBtn.setEnabled(false);
             if(MenuStatu == MENU_STATUS.CLOSE) {
-                for (int i = 1;i < getChildCount();i++) {
-                    final View view = getChildAt(i);
-                    unfoldAnimation(view,i);
-                }
+                openMenu();
             } else {
-                for (int i = 1;i < getChildCount();i++) {
-                    final View view = getChildAt(i);
-                    foldAnimation(view,i);
-                }
+               closeMenu();
             }
         }
     }
@@ -266,16 +289,39 @@ public class SphereMenu extends ViewGroup implements View.OnClickListener{
      * @return
      */
     private boolean isCrash(View v,MotionEvent event) {
-        final float touchX = event.getX();
-        final float touchY = event.getY();
+        final float touchX = event.getX(event.getActionIndex());
+        final float touchY = event.getY(event.getActionIndex());
         final float x = v.getX();
         final float y = v.getY();
         final int w = v.getMeasuredWidth();
         final int h = v.getMeasuredHeight();
-        Log.v("info","touchX:"+touchX+",touchY:"+touchY+",w:"+w+",h:"+h+",x:"+x+",y:"+y);
         if(touchX > x && touchX < x+w && touchY > y && touchY < y + h) {
             return true;
         }
         return false;
     }
+
+    public void closeMenu() {
+        for (int i = 1;i < getChildCount();i++) {
+            final View view = getChildAt(i);
+            foldAnimation(view,i);
+        }
+    }
+
+    public void openMenu() {
+        for (int i = 1;i < getChildCount();i++) {
+            final View view = getChildAt(i);
+            view.setVisibility(View.VISIBLE);
+            unfoldAnimation(view,i);
+        }
+    }
+
+    public void addOnSphereMenuClickListener(onSphereMenuItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface onSphereMenuItemClickListener {
+        void onClick(View v,int pos);
+    }
+
 }
